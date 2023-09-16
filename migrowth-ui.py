fcfb2abf764db13547f81f1f3c07bdcae72451f7
sarpermelikertekin <sakarama1@hotmail.com>
2023-09-16 17:39:43 +0200

    Structuring

--- migrowth-ui.py
+++ migrowth-ui.py
@@ -2,20 +2,24 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 
+########## States ##########
 def _init_state():
-    st.session_state.setdefault("input_fields", [""])
-    st.session_state.setdefault("shopping_items", [])
-    st.session_state.setdefault("ingridients", ["Fish", "Tomato", "Onion"])
+
+    ########## Shop Tab ##########
+    st.session_state.setdefault("shop_input_fields", [""])
+    st.session_state.setdefault("shop_shopping_items", [])
+
+    st.session_state.setdefault("shop_show_fields", True)
+
+    ########## Cook Tab ##########
     st.session_state.setdefault(
-        "products", ["MBudget Fish", "MBudget Tomato", "MBudget Onion"])
+        "cook_ingridients", ["Fish", "Tomato", "Onion"])
+    st.session_state.setdefault(
+        "cook_products", ["MBudget Fish", "MBudget Tomato", "MBudget Onion"])
 
-    # Flag to control visibility of input fields
-    st.session_state.setdefault("show_fields", True)
-    st.session_state.setdefault("show_ingridients", False)
-    st.session_state.setdefault("show_products", False)
-
-
-# The main function to run the app
+    ########## SideBar ##########
+    st.session_state.setdefault("cook_show_ingridients", False)
+    st.session_state.setdefault("cook_show_products", False)
 
 
 def main():
@@ -23,63 +27,61 @@ def main():
     # Initialize session state
     _init_state()
 
-    selected2 = option_menu(None, ["Shop", "Cook"],
-                            icons=["shop", "list-task"],
-                            menu_icon="cast", default_index=0, orientation="horizontal")
+    tab = option_menu(None, ["Shop", "Cook"],
+                      icons=["shop", "list-task"],
+                      menu_icon="cast", default_index=0, orientation="horizontal")
 
-    st.title(selected2)
+    st.title(tab)
 
+    ########## SideBar ##########
     st.sidebar.title("User Preferences")
     st.sidebar.write("Let us recommend the best products for you")
 
-    # Add a slider to the sidebar
+    # Sliders
     sustainability_score = st.sidebar.slider('Sustainability', 1, 5, 3)
-
-    # Add a slider to the sidebar
     price_score = st.sidebar.slider('Price', 1, 5, 3)
 
-    if selected2 == 'Shop':
-
-        # Show the subtitle, input fields, and buttons only if show_fields is True
-        if st.session_state.show_fields:
+    if tab == 'Shop':
+        # Show the subtitle, input fields, and buttons only if shop_show_fields is True
+        if st.session_state.shop_show_fields:
             st.subheader("What do you want to buy?")
 
             # Display existing input fields
-            for idx, field in enumerate(st.session_state.input_fields):
-                st.session_state.input_fields[idx] = st.text_input(
+            for idx, field in enumerate(st.session_state.shop_input_fields):
+                st.session_state.shop_input_fields[idx] = st.text_input(
                     f"Item {idx + 1}", field)
 
             # Add a new input field when the button is clicked
             if st.button("Add another item"):
-                st.session_state.input_fields.append("")
+                st.session_state.shop_input_fields.append("")
 
             # Button to submit items
             if st.button("Submit"):
                 # Only add non-empty items
-                st.session_state.shopping_items += [
-                    item for item in st.session_state.input_fields if item]
+                st.session_state.shop_shopping_items += [
+                    item for item in st.session_state.shop_input_fields if item]
                 # Clear all input fields and buttons
-                st.session_state.input_fields = []
-                st.session_state.show_fields = False  # Hide input fields and buttons
+                st.session_state.shop_input_fields = []
+                st.session_state.shop_show_fields = False
 
         # Display shopping list
         st.subheader("Shopping List:")
-        for item in st.session_state.shopping_items:
+        for item in st.session_state.shop_shopping_items:
             st.write(item)
 
-    if selected2 == 'Cook':
+    if tab == 'Cook':
         st.subheader("What do you want to cook?")
         st.text_input("")
         if st.button("Create Ingridients"):
-            st.session_state.show_ingridients = True  # Show ingridients
-        if st.session_state.show_ingridients:
+            st.session_state.cook_show_ingridients = True  # Show cook_ingridients
+        if st.session_state.cook_show_ingridients:
             st.subheader("Ingridients:")
-            for item in st.session_state.ingridients:
+            for item in st.session_state.cook_ingridients:
                 st.write(item)
             if st.button("Show Products"):
-                st.session_state.show_products = True  # Show products
+                st.session_state.cook_show_products = True  # Show cook_products
                 st.subheader("Products:")
-                for item in st.session_state.products:
+                for item in st.session_state.cook_products:
                     st.write(item)
 
 
