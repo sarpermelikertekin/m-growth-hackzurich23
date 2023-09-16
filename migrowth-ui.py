baf792665bbd18bde8bb4d97f38c409a8f92d3c3
sarpermelikertekin <sakarama1@hotmail.com>
2023-09-16 18:42:34 +0200

    UI finalized

--- migrowth-ui.py
+++ migrowth-ui.py
@@ -17,7 +17,11 @@ def _init_state():
     st.session_state.setdefault(
         "cook_products", ["MBudget Fish", "MBudget Tomato", "MBudget Onion"])
 
+    ########## Stars Tab ##########
+    st.session_state.setdefault("points", 50)  # Added for Stars tab
+
     ########## SideBar ##########
+    st.session_state.setdefault("cook_show_init", True)
     st.session_state.setdefault("cook_show_ingridients", False)
     st.session_state.setdefault("cook_show_products", False)
 
@@ -27,8 +31,8 @@ def main():
     # Initialize session state
     _init_state()
 
-    tab = option_menu(None, ["Shop", "Cook"],
-                      icons=["shop", "list-task"],
+    tab = option_menu(None, ["Shop", "Cook", "Stars"],
+                      icons=["shop", "list-task", "star"],
                       menu_icon="cast", default_index=0, orientation="horizontal")
 
     st.title(tab)
@@ -40,6 +44,8 @@ def main():
     # Sliders
     sustainability_score = st.sidebar.slider('Sustainability', 1, 5, 3)
     price_score = st.sidebar.slider('Price', 1, 5, 3)
+
+    ########## Shop Tab ##########
 
     if tab == 'Shop':
         # Show the subtitle, input fields, and buttons only if shop_show_fields is True
@@ -63,26 +69,47 @@ def main():
                 # Clear all input fields and buttons
                 st.session_state.shop_input_fields = []
                 st.session_state.shop_show_fields = False
+                st.subheader("Shopping List:")
 
         # Display shopping list
-        st.subheader("Shopping List:")
         for item in st.session_state.shop_shopping_items:
             st.write(item)
 
+    ########## Cook Tab ##########
+
     if tab == 'Cook':
-        st.subheader("What do you want to cook?")
-        st.text_input("")
-        if st.button("Create Ingridients"):
-            st.session_state.cook_show_ingridients = True  # Show cook_ingridients
+        if st.session_state.cook_show_init:
+            st.subheader("What do you want to cook?")
+            st.text_input("")
+            if st.button("Give me the Ingridients"):
+                st.session_state.cook_show_ingridients = True
         if st.session_state.cook_show_ingridients:
             st.subheader("Ingridients:")
             for item in st.session_state.cook_ingridients:
                 st.write(item)
             if st.button("Show Products"):
-                st.session_state.cook_show_products = True  # Show cook_products
-                st.subheader("Products:")
-                for item in st.session_state.cook_products:
-                    st.write(item)
+                st.session_state.cook_show_products = True
+        if st.session_state.cook_show_products:
+            st.subheader("Products:")
+            for item in st.session_state.cook_products:
+                st.write(item)
+
+    ########## Stars Tab ##########
+
+    if tab == 'Stars':
+        st.subheader("Collect your reward")
+
+        st.write("Total Points: " + str(st.session_state.points))
+
+        cards = [
+            {"name": "Tee", "points_needed": 30},
+            {"name": "Kaffe", "points_needed": 50},
+            {"name": "Urlaub", "points_needed": 70},
+        ]
+
+        for card in cards:
+            st.markdown(f"### {card['name']}")
+            st.markdown(f"Points needed: {card['points_needed']}")
 
 
 # Call the main function
